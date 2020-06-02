33d6b188afce90add36fa7b700fef4fd71ca1350
matned <mat.niedbal@yahoo.com>
2020-06-02 19:21:01 +0200

    DogDao refactor no1

--- src/main/java/eu/mrndesign/matned/searchEngine/data/hibernate/dao/DogDao.java
+++ src/main/java/eu/mrndesign/matned/searchEngine/data/hibernate/dao/DogDao.java
@@ -11,9 +11,14 @@ import org.hibernate.SessionFactory;
 import javax.persistence.criteria.*;
 import java.util.*;
 
-public class DogDao implements DaoInterface<Dog>{
+import static eu.mrndesign.matned.searchEngine.data.statics.Data.*;
 
-    private static final int MAX_RESULTS_ON_SCREEN = 100;
+public class DogDao implements DaoInterface<Dog> {
+
+    public static final String ALL_RACES_SHOW = "ALL";
+    public static final String MALE = "Male";
+    public static final String FEMALE = "Female";
+
     private int firstResult;
     private int lastResult;
 
@@ -49,8 +54,6 @@ public class DogDao implements DaoInterface<Dog>{
         this.orderInterpreter = orderInterpreter;
         firstResult = MAX_RESULTS_ON_SCREEN * pageNo;
         lastResult = firstResult + MAX_RESULTS_ON_SCREEN;
-
-
         selectList = new LinkedList(selectInterpreter.getFieldNameList());
         initializeCriteria();
     }
@@ -60,48 +63,81 @@ public class DogDao implements DaoInterface<Dog>{
         String orderBy = orderInterpreter.orderBy() != null ? orderInterpreter.orderBy() : "dogId";
         List<Dog> result = new LinkedList<>();
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        try(Session session = sessionFactory.openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             CriteriaBuilder cb = session.getCriteriaBuilder();
             CriteriaQuery<Dog> criteriaQuery = cb.createQuery(Dog.class);
             Root<Dog> rootTable = criteriaQuery.from(Dog.class);
-            criteriaQuery.select(rootTable)
-//                    .multiselect(selectList.stream().map(rootTable::get).collect(Collectors.toList()))
-                    //TODO how to create multiple constructors in entity for custom multiselect query ????
-                    .where(
-                            cb.and(
-                                    cb.and(
-                                            cb.like(rootTable.get("dogName"), dogName),
-                                            cb.like(rootTable.get("ownerName"), ownerName),
-                                            cb.like(rootTable.get("ownerLastName"), ownerLastName)
-                                            ),
-                                    cb.between(rootTable.get("dogId"), dogId1, dogId2),
-                                    cb.or(
-                                            cb.equal(rootTable.get("dogGender"), dogGender1),
-                                            cb.equal(rootTable.get("dogGender"), dogGender2)
-
-                                            ),
-                                    cb.between(rootTable.get("dogRace"), dogRace1,dogRace2),
-                                    cb.between(rootTable.get("dogAge"), dogAge1, dogAge2),
-                                    cb.or(
-                                            cb.equal(rootTable.get("isDogPureRace"), isPureRace1? 1 : 0),
-                                            cb.equal(rootTable.get("isDogPureRace"), isPureRace2? 1 : 0)
-
-                                            ),
-                                    cb.between(rootTable.get("dogWeight"), dogWeight1, dogWeight2)
-                            )
-                    );
+            whereStatement(cb, criteriaQuery, rootTable);
             criteriaQuery.orderBy(orderInterpreter.isDesc() ? cb.desc(rootTable.get(orderBy)) : cb.asc(rootTable.get(orderBy)));
             result.addAll(session.createQuery(criteriaQuery)
                     .setFirstResult(firstResult)
                     .setMaxResults(lastResult)
                     .list());
-        }
-        catch (HibernateException e){
+        } catch (HibernateException e) {
             e.printStackTrace();
         }
-
-        changeSelect(result); //TODO temporary until multi select works
+        changeSelect(result);
         return result;
+    }
+
+    @Override
+    public List<String> listOfFields() {
+        return Arrays.asList("NUMBER::dogId::",
+                "VARCHAR::dogName::",
+                "CHECKBOX::dogGender::M::F",
+                "NUMBER::dogAge::",
+                "ENUM::dogRace::SHEPPARD::TERRIER::GOLDEN_RETRIEVER::BASSET::GREYHOUND::CHIHUAHUA::MOPS::HUSKY::DOG::SPANIEL",
+                "NUMBER::dogWeight::",
+                "BOOLEAN::isDogPureRace::",
+                "VARCHAR::ownerName::",
+                "VARCHAR::ownerLastName::");
+    }
+
+
+
+    /*
+    PRIVATE METHODS
+    -------------------------------
+    Data list indexes:
+    dogName 0,1
+    ownerName 2,3
+    ownerLastName 4,5
+    dogRace 6,7
+    dogId 8,9
+    dogAge 10,11
+    dogWeight 12,13
+    dogGender 14,15
+    isPureRace 16,17
+
+     */
+
+    private void whereStatement(CriteriaBuilder cb, CriteriaQuery<Dog> criteriaQuery, Root<Dog> rootTable) {
+        criteriaQuery.select(rootTable)
+//                    .multiselect(selectList.stream().map(rootTable::get).collect(Collectors.toList()))
+                //TODO how to create multiple constructors in entity for custom multiselect query ????
+                .where(
+                        cb.and(
+                                cb.and(
+                                        cb.like(rootTable.get("dogName"), dogName),
+                                        cb.like(rootTable.get("ownerName"), ownerName),
+                                        cb.like(rootTable.get("ownerLastName"), ownerLastName)
+                                ),
+                                cb.between(rootTable.get("dogId"), dogId1, dogId2),
+                                cb.or(
+                                        cb.equal(rootTable.get("dogGender"), dogGender1),
+                                        cb.equal(rootTable.get("dogGender"), dogGender2)
+
+                                ),
+                                cb.between(rootTable.get("dogRace"), dogRace1, dogRace2),
+                                cb.between(rootTable.get("dogAge"), dogAge1, dogAge2),
+                                cb.or(
+                                        cb.equal(rootTable.get("isDogPureRace"), isPureRace1 ? 1 : 0),
+                                        cb.equal(rootTable.get("isDogPureRace"), isPureRace2 ? 1 : 0)
+
+                                ),
+                                cb.between(rootTable.get("dogWeight"), dogWeight1, dogWeight2)
+                        )
+                );
     }
 
     private void changeSelect(List<Dog> result) {
@@ -118,25 +154,10 @@ public class DogDao implements DaoInterface<Dog>{
         }
     }
 
-    @Override
-    public List<String> listOfFields() {
-        return Arrays.asList("NUMBER::dogId::",
-                "VARCHAR::dogName::",
-                "CHECKBOX::dogGender::M::F",
-                "NUMBER::dogAge::",
-                "ENUM::dogRace::SHEPPARD::TERRIER::GOLDEN_RETRIEVER::BASSET::GREYHOUND::CHIHUAHUA::MOPS::HUSKY::DOG::SPANIEL",
-                "NUMBER::dogWeight::",
-                "BOOLEAN::isDogPureRace::",
-                "VARCHAR::ownerName::",
-                "VARCHAR::ownerLastName::");
-    }
-
     private void initializeCriteria() {
-//        firstResult = 0; TODO
-//        lastResult = firstResult + MAX_RESULTS_ON_SCREEN;
         dogName = item;
-        dogGender1 = "Male";
-        dogGender2 = "Female";
+        dogGender1 = MALE;
+        dogGender2 = FEMALE;
         isPureRace1 = true;
         isPureRace2 = false;
         dogId1 = advancedInterpreter.getIntegers().get(8);
@@ -145,52 +166,71 @@ public class DogDao implements DaoInterface<Dog>{
         dogAge2 = advancedInterpreter.getIntegers().get(11);
         dogWeight1 = advancedInterpreter.getIntegers().get(12);
         dogWeight2 = advancedInterpreter.getIntegers().get(13);
-        ownerName = "%";
-        ownerLastName = "%";
+        ownerName = LIKE_ALL_SIGN;
+        ownerLastName = LIKE_ALL_SIGN;
         dogRace1 = DogRace.AA;
         dogRace2 = DogRace.ZZ;
         getAdvancedData();
     }
 
     private void getAdvancedData() {
+        getDogName();
+        getOwnerNames();
+        getDogRace();
+        getPureRaceFlag();
+        getGender();
+    }
+
+    private void getDogName() {
         if (advancedInterpreter.getChecksList().get(0)) {
             dogName = item;
-            ownerName = "%";
-            ownerLastName = "%";
+            ownerName = LIKE_ALL_SIGN;
+            ownerLastName = LIKE_ALL_SIGN;
         }
+    }
+
+    private void getOwnerNames() {
         if (advancedInterpreter.getChecksList().get(2)) {
-            dogName = "%";
+            dogName = LIKE_ALL_SIGN;
             ownerName = item;
-            ownerLastName = "%";
+            ownerLastName = LIKE_ALL_SIGN;
         }
         if (advancedInterpreter.getChecksList().get(4)) {
-            dogName = "%";
-            ownerName = "%";
+            dogName = LIKE_ALL_SIGN;
+            ownerName = LIKE_ALL_SIGN;
             ownerLastName = item;
         }
-        if (!advancedInterpreter.getOptionsList().get(6).equals("ALL")){
+    }
+
+    private void getDogRace() {
+        if (!advancedInterpreter.getOptionsList().get(6).equals(ALL_RACES_SHOW)) {
             dogRace1 = DogRace.valueOf(advancedInterpreter.getOptionsList().get(6));
             dogRace2 = DogRace.valueOf(advancedInterpreter.getOptionsList().get(6));
         }
-        if(advancedInterpreter.getChecksList().get(16) || advancedInterpreter.getChecksList().get(17)) {
+    }
+
+    private void getPureRaceFlag() {
+        if (advancedInterpreter.getChecksList().get(16) || advancedInterpreter.getChecksList().get(17)) {
             if (advancedInterpreter.getChecksList().get(16) && !advancedInterpreter.getChecksList().get(17)) {
                 isPureRace1 = true;
                 isPureRace2 = true;
             }
-           if (!advancedInterpreter.getChecksList().get(16) && advancedInterpreter.getChecksList().get(17)) {
+            if (!advancedInterpreter.getChecksList().get(16) && advancedInterpreter.getChecksList().get(17)) {
                 isPureRace1 = false;
                 isPureRace2 = false;
             }
         }
+    }
 
+    private void getGender() {
         if (advancedInterpreter.getChecksList().get(14) || advancedInterpreter.getChecksList().get(15)) {
             if (advancedInterpreter.getChecksList().get(14) && !advancedInterpreter.getChecksList().get(15)) {
-                dogGender1 = "Male";
-                dogGender2 = "Male";
+                dogGender1 = MALE;
+                dogGender2 = MALE;
             }
             if (!advancedInterpreter.getChecksList().get(14) && advancedInterpreter.getChecksList().get(15)) {
-                dogGender1 = "Female";
-                dogGender2 = "Female";
+                dogGender1 = FEMALE;
+                dogGender2 = FEMALE;
             }
         }
     }
---
+++ src/main/java/eu/mrndesign/matned/searchEngine/data/statics/Data.java
@@ -0,0 +1,8 @@
+package eu.mrndesign.matned.searchEngine.data.statics;
+
+public class Data {
+
+    public static final int MAX_RESULTS_ON_SCREEN = 100;
+    public static final String LIKE_ALL_SIGN = "%";
+
+}
